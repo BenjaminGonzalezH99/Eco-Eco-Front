a2a0b1df23a761aff0028262b2a9c8654be1deb4
BenjaminGonzalezH99 <Benjamin.Git@outlook.cl>
2024-07-10 14:29:06 -0600

    conexion api formspree para correo automatico

--- front proyecto/Front/src/App.jsx
+++ front proyecto/Front/src/App.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Components/NavBar/NavBar";
 import Inicio from "./Pages/Inicio";
 import Contacto from "./Pages/Contacto/Contacto";
 import Comparador from "./Pages/Comparador/Comparador";
-import Catalogo from "./Pages/Catalogo";
-import AboutUs from "./Pages/AboutUs/AboutUs";
+/* import Catalogo from "./Pages/Catalogo";
+import AboutUs from "./Pages/AboutUs/AboutUs"; */
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
         <Route path="/" element={<Inicio />}></Route>
         <Route path="/contacto" element={<Contacto />}></Route>
         <Route path="/comparador" element={<Comparador />}></Route>
-        <Route path="/about-us" element={<AboutUs />}></Route>
-        <Route path="/catalogo" element={<Catalogo />}></Route>
+        {/* <Route path="/about-us" element={<AboutUs />}></Route>
+        <Route path="/catalogo" element={<Catalogo />}></Route> */}
       </Routes>
     </BrowserRouter>
   );
